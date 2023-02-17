4ddd12a5ac9606863581557dbb7391c93cbcd288
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-17 10:56:17 -0400

    add schemas

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,3 +1,10 @@
 import testimonials from "./testimonials";
+import about from './about';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import works from './works';
 
-export const schemaTypes = [testimonials];
+export const schemaTypes = [about, brands, contact, experiences, skills, testimonials, workExperience, works];

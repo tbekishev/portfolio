f4d241d4a57dccbde57804bce8565388a03f461e
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-18 16:47:14 -0400

    add objects in About component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Header from './container/Header/Header';
+import About from './container/About/About';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   );
 }

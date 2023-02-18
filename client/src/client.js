0ed26323058a9f2992a551e4b84cf09d13dc3677
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-18 19:15:31 -0400

    fetch abouts from sanity DB

--- client/src/client.js
+++ client/src/client.js
@@ -1,12 +1,12 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
-  projectID: process.env.REACT_APP_SANITY_PROJECT_ID,
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2023-02-18',
-  useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN
+  useCdn: false,
+  token: 'skAGzm3ZCvuyj9FWlkndrwwFi9VvbtJXpsRFVoBQofbxARKxBUtNakvdZAxMCLrvt8CLfYhkBNC7AbH4qxBg1UjkM2TL5QDzbI7J1492IPq42YRPcvzmM2YyKC6AYI8BBimjMWGL57onL5LDQ7htAdpR7INWhsdj2H4qMOgmfLp4hpZ51C4d'
 });
 
 const builder = imageUrlBuilder(client);
--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
-
-const abouts = [
-  { title: 'Web Development', 
-    description: 'I am a good web developer.',
-    imgUrl: images.about01 
-  },
-  { title: 'Frontend Development', 
-    description: 'I am a good web developer.',
-    imgUrl: images.about02 
-  },
-  { title: 'Backend Development', 
-    description: 'I am a good web developer.',
-    imgUrl: images.about03 
-  },
-  { title: 'Web Development', 
-    description: 'I am a good web developer.',
-    imgUrl: images.about04 
-  }
-]
+import { client, urlFor } from '../../client';
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client
+      .fetch(query)
+      .then((data) => {
+        setAbouts(data);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>I Know That <span>Good Apps</span> <br /> means <span>Good Business</span> </h2>
@@ -35,7 +33,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title}/>
+            <img src={urlFor(about.imgUrl)} alt={about.title}/>
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>

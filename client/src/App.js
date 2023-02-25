908629b1d706289cbaada6b6b2492a9147333da1
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-25 18:36:49 -0400

    add react-tooltip and Skills component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Header from './container/Header/Header';
-import About from './container/About/About';
-import Work from './container/Work/Work';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar/>
       <Header/>
       <About/>
       <Work/>
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
